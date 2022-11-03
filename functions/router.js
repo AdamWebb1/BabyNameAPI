b4ee6a50a2c19aac56f7296f1319ac40428c44f0
Adam Webb <adam.webb@mrjaffesclass.com>
2022-11-03 12:46:38 -0700

    initial commit

---
+++ functions/router.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const after_year = require('./routes/baby_name_year_after');
+const before_year = require('./routes/baby_name_year_before');
+const name_year = require('./routes/baby_name_year');
+const name = require('./routes/baby_name');
+const year_end = require('./routes/baby_year_end');
+const year_name = require('./routes/baby_year_name');
+const year_start = require('./routes/baby_year_start');
+const year = require('./routes/baby_year');
+
+
+router.get("/baby-name/:name/after/:afterYear", after_year);
+router.get("/baby-name/:name/before/:beforeYear", before_year);
+router.get("/baby-name/:name/:year", name_year);
+router.get("/baby-name/:name", name);
+router.get("/baby-year/:year/:name", year_name);
+router.get("/baby-year-start/:year/:letter", year_start);
+router.get("/baby-year-end/:year/:letter", year_end);
+router.get("/baby-year/:year", year);
+
+
+module.exports = router;
---
+++ functions/routes/baby_name_after_year.js
@@ -0,0 +1,7 @@
+const {byName, byYear} = require("us-baby-names");
+
+const baby_name_after_year = (req, res) => {
+    const name = req.params.name
+    const year = req.params.year
+    
+}
